eefebc0f992c5f0842378a4d1072eea6ed2b2b36
garvit-010 <garvitaudichya010@gmail.com>
2025-10-18 15:23:36 +0530

    Initial Commit

---
+++ App/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import joblib
+
+model = load_model(r"D:\Projects\IMDB-Movie-Review-Sentiment-Analysis\Analysis\lstm_w2v_model.keras")
+tokenizer = joblib.load(r"D:\Projects\IMDB-Movie-Review-Sentiment-Analysis\Analysis\tokenizer.pkl")
+
+st.title("🎬 Movie Review Sentiment Analysis")
+st.write("Enter a movie review below to predict sentiment.")
+
+text = st.text_area("Review Text")
+
+if st.button("Predict"):
+    if text.strip():
+        seq = tokenizer.texts_to_sequences([text])
+        pad = pad_sequences(seq, maxlen=200)
+        pred = model.predict(pad)[0][0]
+        sentiment = "😊 Positive" if pred > 0.5 else "😞 Negative"
+        st.success(f"**Sentiment:** {sentiment} (Score: {pred:.4f})")
+    else:
+        st.warning("Please enter a review.")
